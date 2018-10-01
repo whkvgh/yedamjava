60f46c3670c35a3b7c31636542eb0226351de8ea
User <User@User-PC>
2018-10-01 17:44:48 +0900

    zzzzzzzzzz

---
+++ yedamjava/src/Dao/outList.java
@@ -0,0 +1,42 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+import Bean.ItemListBean;
+
+public class outList {
+	static Connection conn = null;
+	static PreparedStatement pstmt;
+	static ResultSet rs;
+	
+	public void db_con() {
+		try {
+			
+			String user ="item";
+			String pw = "item";
+			String url = "jdbc:oracle:thin:@192.168.0.56:1521:xe";
+
+		    Class.forName("oracle.jdbc.driver.OracleDriver");        
+		    conn = DriverManager.getConnection(url, user, pw);
+		         
+		    System.out.println("Database에 연결되었습니다.\n");
+		}catch (Exception e) {
+	         e.printStackTrace();
+	    }
+	}
+
+	public void delete() {
+		Scanner sc  = new Scanner(System.in);
+		ItemListBean item = new ItemListBean();
+		
+		System.out.println("=================");
+		System.out.println("출고할 품목을 선택하세요");
+		String n = sc.nextLine();
+		sc.close();
+		
+	}
+}
--- yedamjava/src/Model/Start.java
+++ yedamjava/src/Model/Start.java
@@ -5,5 +5,7 @@ import Dao.db_con;
 public class Start {
 	public static void main(String[] args) {
 		db_con con = new db_con();
+		
+	
 	}
 }
