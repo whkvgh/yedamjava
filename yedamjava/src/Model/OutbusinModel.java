bbc07275b628cedfe025c2ce6a0d20985cb3a214
User <User@User-PC>
2018-10-10 17:56:13 +0900

    이게머냐

--- yedamjava/src/Model/OutbusinModel.java
+++ yedamjava/src/Model/OutbusinModel.java
@@ -1,22 +1,37 @@
 package Model;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import Bean.OutbusinBean;
 import Dao.OutlistDao;
 
 public class OutbusinModel {
-	private OutlistDao dao;
 	private OutbusinBean bean;
+	private OutlistDao dao;
 	private ResultSet rs;
 	
-	public void outbusin() {
+	public void outbusinList() {
 		dao = new OutlistDao();
 		bean = new OutbusinBean();
-		rs = dao.
+		rs = dao.out_bname();
+		
 		System.out.println("판매할 업체 정보");
-		System.out.println("업체코드\t업체명\\t주소\\t연락처\\t대표자명");
-	
+		System.out.println("업체코드\t업체명\t주소\t연락처\t대표자명");
+		
+		try {
+			if(rs.next()) {
+				bean.setBusin_code(rs.getString(1));
+				bean.setBusin_name(rs.getString(2));
+				bean.setBusin_addr(rs.getString(3));
+				bean.setBusin_num(rs.getString(4));
+				bean.setCeo(rs.getString(5));
+				System.out.println(busin_code + "\t" + busin_name + "\t" + busin_addr +"\t" + busin_num + "\t" + ceo );
+			
+			} else System.out.println("선택한 업체가 없습니다.");
+		} catch (SQLException e) {
+	         e.printStackTrace();
+	      }
 	}
 }
 	
--- yedamjava/src/Model/OutlistModel.java
+++ yedamjava/src/Model/OutlistModel.java
@@ -5,28 +5,25 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 import Bean.ItemListBean;
+import Bean.OutbusinBean;
 import Dao.OutlistDao;
 
 public class OutlistModel {
-	private OutlistDao dao;
-	private ItemListBean bean;
-	private ResultSet rs;
+	OutbusinBean bean = new OutbusinBean();
 	Scanner sc = new Scanner(System.in);
 	
+	
 	public void OutItem() {
-		dao = new OutlistDao();
-		rs = dao.OutItem();
-		if(rs.next()){
-			do {
-				ItemListBean bean = new ItemListBean();
-				System.out.println("판매할 상품명을 입력하세요");
-				bean.setSub_group(rs.getString("sub_group"));
-				System.out.println("판매할 상품수량을 입력하세요");
-				System.out.println("판매할 업체명을 입력하세요");
-				bean.setStore_code(rs.getString("busin_code"));
-			}while (rs.next());
-		}
+		System.out.println("====================");
+		System.out.println("판매할 업체명을 입력하세요");
+		bean.setBusin_name(sc.next());
+		System.out.println("판매할 제품을 입력하세요");
+		System.out.println("판매할 수량을 입력하세요");
+	}
+	
+		
+		
 	} catch (SQLException e) {
 		e.printStackTrace();
 	}
-}
+
