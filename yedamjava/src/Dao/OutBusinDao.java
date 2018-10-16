6a30b4dbbbb342ed3a0943ef80056e5de38761fd
User <User@User-PC>
2018-10-16 17:50:33 +0900

    ac!

---
+++ yedamjava/src/Dao/OutBusinDao.java
@@ -0,0 +1,57 @@
+package Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Bean.OutbusinBean;
+
+public class OutBusinDao {
+	private Connection conn = null;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	private OutbusinBean bean;
+
+	public OutBusinDao() {
+		try {
+
+			String user = "item";
+			String pw = "item";
+			String url = "jdbc:oracle:thin:@192.168.0.56:1521:xe";
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, user, pw);
+
+			System.out.println("Database에 연결되었습니다.\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void InsertOutBusin() {
+		String sql = "insert into out_busin" + "values(?,?,?,?,?)";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, bean.getBusin_code());
+			pstmt.setString(2, bean.getBusin_name());
+			pstmt.setString(3, bean.getBusin_addr());
+			pstmt.setString(4, bean.getBusin_num());
+			pstmt.setString(5, bean.getCeo());
+
+			pstmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void editOutBusin() {
+		
+	}
+	
+	public void deleteOutlist() {
+		
+	}
+}
